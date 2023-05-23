3ec7fa3e1e56761425a3db62efdf63368ac81b90
Linuss <tade3d@gmail.com>
2023-05-23 08:29:35 +0200

    .

--- 04-fetch/src/services/API.ts
+++ 04-fetch/src/services/API.ts
@@ -1,19 +1,19 @@
-import { IResource } from '../types'
+import { IResource } from "../types";
 
 export const getResource = async (resource: string) => {
-	// fetch resource
-	const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+  // fetch resource
+  const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`);
 
-	// make fetch behave
-	if (!res.ok) {
-		throw new Error(`${res.status} ${res.statusText}`)
-	}
+  // make fetch behave
+  if (!res.ok) {
+    throw new Error(`${res.status} ${res.statusText}`);
+  }
 
-	// parse response as json
-	const payload = await res.json() as IResource[]
+  // parse response as json
+  const payload = (await res.json()) as IResource[];
 
-	// fake slow api
-	await new Promise(r => setTimeout(r, 1500))
+  // fake slow api
+  await new Promise((r) => setTimeout(r, 1500));
 
-	return payload
-}
+  return payload;
+};
