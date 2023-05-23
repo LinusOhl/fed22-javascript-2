53c6fe41b446beafa9219e7c9654992450ad2c7f
Linuss <tade3d@gmail.com>
2023-05-23 13:37:42 +0200

    .

--- 05-react-weather/src/components/SearchCity.tsx
+++ 05-react-weather/src/components/SearchCity.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const SearchCity = () => {
-	return (
-		<div id="search-wrapper">
-			<form id="search-form">
-				<div className="input-group">
-					<input
-						type="text"
-						className="form-control"
-						placeholder="Enter city to search for" aria-label="City" aria-details="Search for city to show current weather for."
-					/>
+  return (
+    <div id="search-wrapper">
+      <form id="search-form">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter city to search for"
+            aria-label="City"
+            aria-details="Search for city to show current weather for."
+          />
 
-					<button
-						type="submit"
-						className="btn btn-success"
-					>🔍</button>
-				</div>
-			</form>
-		</div>
-	)
-}
+          <button type="submit" className="btn btn-success">
+            🔍
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default SearchCity
+export default SearchCity;
--- 05-react-weather/src/services/owmapi.ts
+++ 05-react-weather/src/services/owmapi.ts
@@ -1,11 +1,11 @@
 /**
  * Open Weather Map API
  */
-import axios from 'axios'
-import { ICurrentWeather } from '../types'
+import axios from "axios";
+import { ICurrentWeather } from "../types";
 
-const API_KEY = import.meta.env.VITE_APP_OWM_API_KEY
-const BASE_URL = "https://api.openweathermap.org/data/2.5"
+const API_KEY = import.meta.env.VITE_APP_OWM_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 /**
  * Get current weather for a city.
@@ -13,13 +13,14 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5"
  * @param {string} query City to get current weather for
  */
 export const getCurrentWeather = async (query: string) => {
-	const response = await axios.get<ICurrentWeather>(`${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`)
+  const response = await axios.get<ICurrentWeather>(
+    `${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`
+  );
 
-	await new Promise(r => setTimeout(r, 1500))
+  await new Promise((r) => setTimeout(r, 1500));
 
-	return response.data
-}
-
+  return response.data;
+};
 
 /**
  * Get current weather for a city.
@@ -27,15 +28,17 @@ export const getCurrentWeather = async (query: string) => {
  * @param {string} query City to get current weather for
  */
 const getCurrentWeatherFetch = async (query: string) => {
-	// get weather for query from OpenWeatherMap API
-	const response = await fetch(`${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`)
+  // get weather for query from OpenWeatherMap API
+  const response = await fetch(
+    `${BASE_URL}/weather?q=${query}&units=metric&appid=${API_KEY}`
+  );
 
-	// convert response from JSON
-	const data = await response.json()
+  // convert response from JSON
+  const data = await response.json();
 
-	// fake slow api
-	// await new Promise(r => setTimeout(r, 1500))
+  // fake slow api
+  // await new Promise(r => setTimeout(r, 1500))
 
-	// return current weather
-	return data
-}
+  // return current weather
+  return data;
+};
