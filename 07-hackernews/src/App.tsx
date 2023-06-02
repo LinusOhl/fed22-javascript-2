f0c1123cc4e0340032d2262db2fde69b8e598f54
Linuss <tade3d@gmail.com>
2023-06-02 14:11:08 +0200

    .

--- 07-hackernews/src/App.tsx
+++ 07-hackernews/src/App.tsx
@@ -1,8 +1,21 @@
+import Container from "react-bootstrap/Container";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navigation from "./components/Navigation";
+import "./assets/scss/App.scss";
+
 const App = () => {
   return (
-    <>
-      <h1>HackerNews</h1>
-    </>
+    <div id="App">
+      <Navigation />
+
+      <Container className="py-3">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/search" element={} /> */}
+        </Routes>
+      </Container>
+    </div>
   );
 };
 
